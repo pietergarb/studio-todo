7f85da3bcd35f85b41b62ab99958aa851678b23f
pietergarb <noreply>
2015-04-30 19:05:56 +0000

    Add component compTryOut1

---
+++ ui/comp-try-out1.reel/comp-try-out1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/comp-try-out1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CompTryOut1
+ * @extends Component
+ */
+exports.CompTryOut1 = Component.specialize(/** @lends CompTryOut1# */ {
+    constructor: {
+        value: function CompTryOut1() {
+            this.super();
+        }
+    }
+});
